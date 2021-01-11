e666c3361dcb0e95fd0b069e3188da3e190864bf
Abdu <abdukr5@gmail.com.com>
2021-01-10 23:21:05 -0600

    implement full CRUD functionality for the /api/coffee endpoint

---
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+using CoffeeShop.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        private readonly ICoffeeRepository _coffeeRepository;
+        public CoffeeController(ICoffeeRepository coffeeRepository)
+        {
+            _coffeeRepository = coffeeRepository;
+        }
+
+        // https://localhost:5001/api/coffee/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_coffeeRepository.GetAllCoffees());
+        }
+        // https://localhost:5001/api/coffee/1
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var coffee = _coffeeRepository.GetACoffee(id);
+            if (coffee == null)
+            {
+                return NotFound();
+            }
+            return Ok(coffee);
+        }
+
+        // https://localhost:5001/api/coffee/
+        [HttpPost]
+        public IActionResult Post(Coffee coffee)
+        {
+            _coffeeRepository.AddCoffee(coffee);
+            return CreatedAtAction("Get", new { id = coffee.Id }, coffee);
+        }
+        // https://localhost:5001/api/coffee/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Coffee coffee)
+        {
+            if (id != coffee.Id)
+            {
+                return BadRequest();
+            }
+
+            _coffeeRepository.UpdateCoffee(coffee);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _coffeeRepository.DeleteCoffe(id);
+            return NoContent();
+        }
+    }
+}
